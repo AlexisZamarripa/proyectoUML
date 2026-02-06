79cd5d5136ad32322ab0e36e887e73ae22f1cd48
JDCAstro1903 <133943864+JDCAstro1903@users.noreply.github.com>
2026-02-06 14:39:10 -0700

    observaciones

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CasosUsoComponent } from './Admin/CasosUso/cu.component';
 import { ProyectosComponent } from './Admin/Proyecto/proyecto.component';
 import { EntrevistaComponent } from './Admin/Entrevista/entrevista.component';
 import { EncuestaComponent } from './Admin/Encuestas/encuesta.component';
+import { ObservacionComponent } from './Admin/Observaciones/observacion.component';
 import { StakeholderComponent } from './Admin/stakeholder/stakeholder.component';
 import { ProcesosComponent } from './Admin/procesos/procesos.component';
 
@@ -15,5 +16,6 @@ export const routes: Routes = [
     { path: 'proyecto/:id/procesos', component: ProcesosComponent },
     { path: 'proyecto/:id/entrevistas', component: EntrevistaComponent },
     { path: 'proyecto/:id/encuestas', component: EncuestaComponent },
+    { path: 'proyecto/:id/observaciones', component: ObservacionComponent },
     { path: '**', redirectTo: '/proyectos' },
 ];
